ac01f2818172f2f2d1f08b0bd762e850a2213e7f
OleksandrLarionov <larionov.alex23@gmail.com>
2024-01-19 15:53:51 +0100

    messages validation updated

--- exam/src/main/java/larionovoleksanr/exam/payloads/NewEmployeeDTO.java
+++ exam/src/main/java/larionovoleksanr/exam/payloads/NewEmployeeDTO.java
@@ -5,12 +5,12 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public record NewEmployeeDTO(
-        @Email
+        @Email(message = "inserisci un indirizzo email valido e nel formato corretto es: john.doe@mail.com")
         String email,
-        @NotEmpty
+        @NotEmpty(message = "il campo del nome è vuoto")
         @Size(min = 2, max = 20, message = "Il Nome deve contenere tra 2 e 20 caratteri")
         String name,
-        @NotEmpty
+        @NotEmpty(message = "il campo del cognome è vuoto")
         @Size(min = 2, max = 20, message = "Il Cognome deve contenere tra 2 e 20 caratteri")
         String surname) {
 }
