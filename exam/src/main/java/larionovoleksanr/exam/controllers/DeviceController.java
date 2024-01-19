3a91ab42c6218c3c368fa1c65c7ea3c607a089c0
OleksandrLarionov <larionov.alex23@gmail.com>
2024-01-19 11:54:50 +0100

    device controller and service

--- exam/src/main/java/larionovoleksanr/exam/controllers/DeviceController.java
+++ exam/src/main/java/larionovoleksanr/exam/controllers/DeviceController.java
@@ -1,9 +1,62 @@
 package larionovoleksanr.exam.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import larionovoleksanr.exam.entities.Dipendente;
+import larionovoleksanr.exam.entities.Dispositivo;
+import larionovoleksanr.exam.payloads.NewDeviceDTO;
+import larionovoleksanr.exam.services.DeviceService;
+import larionovoleksanr.exam.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/devices")
 public class DeviceController {
+    @Autowired
+    private DeviceService deviceService;
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public Page<Dispositivo> getDevices(@RequestParam(defaultValue = "0") int page,
+                                        @RequestParam(defaultValue = "0") int size,
+                                        @RequestParam(defaultValue = "id") String orderBy) {
+        return deviceService.getDevices(page, size, orderBy);
+    }
+
+    @PostMapping
+    public Dispositivo saveBlogpost(@RequestBody NewDeviceDTO payload) {
+        if (payload.idEmployee() == null) {
+            Dispositivo device = new Dispositivo();
+            device.setDeviceType(payload.type());
+            device.setStateOfDevice(payload.stateOfDevice());
+            return deviceService.saveDevice(device);
+        } else {
+            Long employeeId = payload.idEmployee();
+            Dipendente employee = employeeService.findById(employeeId);
+            Dispositivo device = new Dispositivo();
+            device.setEmployee(employee);
+            device.setDeviceType(payload.type());
+            device.setStateOfDevice(payload.stateOfDevice());
+            return deviceService.saveDevice(device);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public Dispositivo findById(@PathVariable Long id) {
+        return deviceService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Dispositivo findByIdAndUpdate(@PathVariable Long id,
+                                         @RequestBody Dispositivo body,
+                                         @RequestBody Dipendente employee) {
+        return deviceService.findByIdAndUpdate(id, body, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void findByIdAndDelete(@PathVariable Long id) {
+        deviceService.delete(id);
+    }
 }
--- exam/src/main/java/larionovoleksanr/exam/services/DeviceService.java
+++ exam/src/main/java/larionovoleksanr/exam/services/DeviceService.java
@@ -1,4 +1,50 @@
 package larionovoleksanr.exam.services;
 
+import larionovoleksanr.exam.entities.Dipendente;
+import larionovoleksanr.exam.entities.Dispositivo;
+import larionovoleksanr.exam.exceptions.NotFoundException;
+import larionovoleksanr.exam.repositories.DispositivoDAO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+@Slf4j
 public class DeviceService {
+    @Autowired
+    private DispositivoDAO dispositivoDAO;
+
+    public Page<Dispositivo> getDevices(int page, int size, String orderBy) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(orderBy));
+        return dispositivoDAO.findAll(pageable);
+    }
+
+
+    public Dispositivo saveDevice(Dispositivo body) {
+        return dispositivoDAO.save(body);
+    }
+
+    public Dispositivo findById(Long id) {
+        return dispositivoDAO.findById(id).orElseThrow(() -> new NotFoundException(id));
+    }
+
+    public void delete(Long id) {
+        Dispositivo found = this.findById(id);
+        dispositivoDAO.delete(found);
+
+    }
+
+    public Dispositivo findByIdAndUpdate(Long id, Dispositivo body, Dipendente employee) {
+        Dispositivo found = this.findById(id);
+        found.setId(id);
+        found.setDeviceType(body.getDeviceType());
+        found.setStateOfDevice(body.getStateOfDevice());
+        return dispositivoDAO.save(found);
+    }
 }
