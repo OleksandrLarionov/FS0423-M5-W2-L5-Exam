3438d8ba4546ef58ff3c4bb7b8d41277f841ca61
OleksandrLarionov <larionov.alex23@gmail.com>
2024-01-19 10:10:49 +0100

    repositories

--- exam/src/main/java/larionovoleksanr/exam/repositories/DipendenteDAO.java
+++ exam/src/main/java/larionovoleksanr/exam/repositories/DipendenteDAO.java
@@ -1,4 +1,9 @@
 package larionovoleksanr.exam.repositories;
 
-public class DipendenteDAO {
+import larionovoleksanr.exam.entities.Dipendente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DipendenteDAO extends JpaRepository<Dipendente, Long> {
 }
